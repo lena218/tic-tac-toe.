96918d0a21141daa947d8c993015258b6d05179f
заморяхина лена <krosovochka@gmail.com>
2026-01-13 17:36:14 +0500

    tictactoe.h

--- tictactoe.h
+++ tictactoe.h
@@ -4,21 +4,19 @@
 #include <vector>
 #include <string>
 
-// Функции отображения
+
 void drawboard(const std::vector<char>& board);
 void showMenu();
 
-// Функции проверки состояния игры
+
 bool checkwin(const std::vector<char>& board, char player);
 bool checkdraw(const std::vector<char>& board);
 
-// Основные игровые функции
-int playGame();
-void startNewGame();
-void restartGame(std::vector<char>& board, char& currentPlayer);
-void playGameWithParams(std::vector<char>& board, char& currentPlayer); // <-- добавлено
 
-// Функции работы с файлами
+void startNewGame(std::vector<char>& board, char& currentPlayer, bool& gameInProgress);
+void playGameWithParams(std::vector<char>& board, char& currentPlayer, bool& gameInProgress);
+
+
 bool saveGame(const std::vector<char>& board, char currentPlayer, const std::string& filename);
 bool loadGame(std::vector<char>& board, char& currentPlayer, const std::string& filename);
 
